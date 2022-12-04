f51e43028577c3cacec654067342761deaad2039
zasfiaclaudia <112447707+zasfiaclaudia@users.noreply.github.com>
2022-12-05 00:05:29 +0700

    Add files via upload

---
+++ ci4/app/Config/Routes.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Config;
+
+// Create a new instance of our RouteCollection class.
+$routes = Services::routes();
+
+// Load the system's routing file first, so that the app and ENVIRONMENT
+// can override as needed.
+if (is_file(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
+/*
+ * --------------------------------------------------------------------
+ * Router Setup
+ * --------------------------------------------------------------------
+ */
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Login');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+// The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
+// where controller filters or CSRF protection are bypassed.
+// If you don't want to define all routes, please use the Auto Routing (Improved).
+// Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
+$routes->setAutoRoute(true);
+
+/*
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+
+// We get a performance increase by specifying the default
+// route since we don't have to scan directories.
+// $routes->get('/', 'Login::index');
+
+// $routes->get('kategori/(:any)', 'admin\kategori::selectWhere/$1');
+
+$routes->group('admin',function($routes){
+
+    $routes->add('/','Admin\adminpage::index');
+    $routes->add('kategori','Admin\kategori::read');
+    $routes->add('kategori/create','Admin\kategori::create');
+    $routes->add('kategori/insert','Admin\kategori::insert');
+    $routes->add('kategori/find/(:any)','Admin\kategori::find/$1');
+    $routes->add('kategori/delete/(:any)','Admin\kategori::delete/$1');
+    $routes->add('kategori/update','Admin\kategori::update');
+
+});
+
+/*
+ * --------------------------------------------------------------------
+ * Additional Routing
+ * --------------------------------------------------------------------
+ *
+ * There will often be times that you need additional routing and you
+ * need it to be able to override any defaults in this file. Environment
+ * based routes is one such time. require() additional route files here
+ * to make that happen.
+ *
+ * You will have access to the $routes object within that file without
+ * needing to reload it.
+ */
+if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}
---
+++ ci4/app/Views/kategori/select.php
@@ -0,0 +1,58 @@
+<?= $this->extend('template/admin') ?>
+
+<?= $this->section('content') ?>
+
+<?php
+
+if (isset($_GET['page_page'])) {
+	$page = $_GET['page_page'];
+	$jumlah = 3;
+	$no = ($jumlah * $page) - $jumlah + 1;
+} else {
+	$no = 1;
+}
+
+?>
+
+
+<div class="row">
+	<div class="col-4">
+		<a class="btn btn-primary" href="<?= base_url('/admin/kategori/create') ?>" role="button">TAMBAH DATA</a>
+	</div>
+	<div class="col">
+		<h3><?= $judul; ?></h3>
+	</div>
+
+</div>
+
+<div class="row mt-2">
+
+	<div class="col">
+		<table class="table">
+
+			<tr>
+				<th>No</th>
+				<th>Kategori</th>
+				<th>Keterangan</th>
+				<th>Hapus</th>
+				<th>Ubah</th>
+			</tr>
+			<?php $no ?>
+			<?php foreach ($kategori as $key => $value) : ?>
+				<tr>
+					<td><?= $no++ ?></td>
+					<td><?= $value['kategori'] ?></td>
+					<td><?= $value['keterangan'] ?></td>
+					<td><a href="<?= base_url() ?>/admin/kategori/delete/<?= $value['idkategori'] ?>"><img src="<?= base_url('/icon/trash.svg') ?>" alt=""></a></td>
+					<td><a href="<?= base_url() ?>/admin/kategori/find/<?= $value['idkategori'] ?>"><img src="<?= base_url('icon/pen.svg') ?>" alt=""></a></td>
+				</tr>
+			<?php endforeach ?>
+
+		</table>
+
+		<?= $pager->links('page', 'bootstrap') ?>
+	</div>
+
+</div>
+
+<?= $this->endSection() ?>
